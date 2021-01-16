55f4b6e4dbd03e356586d945af5e4568c1503d83
Carlos Alberto Pozos Villegas <cpozosvillegas@hotmail.com>
2021-01-16 02:12:36 -0600

    Data class implemented

--- src/data/dataProviders/image.py
+++ src/data/dataProviders/image.py
@@ -1,16 +1,31 @@
+from PIL import Image
+
 class imageProvider(object):
     GO =1
     EOS = 2
     PAD = 0
     MAXWIDTH =400
     MAXHEIGHT = 160
-
+    DATA_DIR = '..\sets\'
 
     """
     docstring
     """
     def __init__(self):
 
+
+        return
+
+    def preprocess(self, type):
+        """
+        docstring
+        """
+
+        return
+        
+
+        
+                
 
         return
 
