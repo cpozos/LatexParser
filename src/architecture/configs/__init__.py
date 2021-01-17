3862a6d8815db00f5ccd8bfd167b6a8109e8a0f6
Carlos Alberto Pozos Villegas <cpozosvillegas@hotmail.com>
2021-01-16 21:06:40 -0600

    Setting up architecture files

--- src/architecture/configs/__init__.py
+++ src/architecture/configs/__init__.py
@@ -1,4 +1,3 @@
 from architecture.configs.encoder_config import EncoderConfig
 from architecture.configs.decoder_config import DecoderConfig
-from architecture.configs.lstm_config import LSTMConfig
 from architecture.configs.model_config import ModelConfig
